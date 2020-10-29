c13b048b5fe8b4a0a63c4ab97dab042ddde1e9bc
Šimon Vyhnis <simon.vyhnis@gmail.com>
2020-10-29 13:51:08 +0100

    fix navigation error2

--- app/src/main/java/com/simcom/ecashier/MainActivity.java
+++ app/src/main/java/com/simcom/ecashier/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_current, R.id.nav_add, R.id.nav_history)
+                R.id.nav_current, R.id.nav_add, R.id.nav_history, R.id.nav_people)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
