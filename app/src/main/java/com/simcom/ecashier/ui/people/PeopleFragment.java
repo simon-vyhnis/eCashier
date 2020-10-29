08631f614b5ce7a972baa1d8fb3256326fcf0a69
Šimon Vyhnis <simon.vyhnis@gmail.com>
2020-10-29 13:32:44 +0100

    fixing navigation problems

--- app/src/main/java/com/simcom/ecashier/ui/people/PeopleFragment.java
+++ app/src/main/java/com/simcom/ecashier/ui/people/PeopleFragment.java
@@ -8,14 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.simcom.ecashier.R;
+import com.simcom.ecashier.ui.addCollection.AddCollectionViewModel;
 
 public class PeopleFragment extends Fragment {
+    private PeopleViewModel viewModel;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_people,container,false);
+        viewModel = new ViewModelProvider(getActivity(), new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())).get(PeopleViewModel.class);
+
         return root;
     }
 }
+
